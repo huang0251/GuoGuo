bad196047d904448dff2506150cb6a13124b1e6e
huang0251 <3174251764@qq.com>
2022-04-20 21:09:14 +0800

    添加了融云SDK

--- app/src/main/java/top/designerSH/guoguo/MainActivity.java
+++ app/src/main/java/top/designerSH/guoguo/MainActivity.java
@@ -2,13 +2,15 @@ package top.designerSH.guoguo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
     }
 }
