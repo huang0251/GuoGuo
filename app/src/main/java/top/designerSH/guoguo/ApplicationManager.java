368a34271d099f53e8f1d5db7e53ad6e56169568
huang0251 <3174251764@qq.com>
2022-04-22 12:31:24 +0800

    添加了融云SDK_new_new

--- app/src/main/java/top/designerSH/guoguo/ApplicationManager.java
+++ app/src/main/java/top/designerSH/guoguo/ApplicationManager.java
@@ -3,11 +3,13 @@ package top.designerSH.guoguo;
 import android.app.Application;
 
 import io.rong.imkit.RongIM;
+import top.designerSH.guoguo.instance.UserInformationManager;
 
 public class ApplicationManager extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        UserInformationManager.mainContext=this;
         RongIM.init(ApplicationManager.this,"vnroth0kvomlo",true);
     }
 }
--- app/src/main/java/top/designerSH/guoguo/MainActivity.java
+++ app/src/main/java/top/designerSH/guoguo/MainActivity.java
@@ -4,13 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.rong.imkit.RongIM;
+import io.rong.imkit.utils.RouteUtils;
+import io.rong.imlib.RongIMClient;
+import top.designerSH.guoguo.instance.UserInformationManager;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //UserInformationManager.login("exply","");
+                UserInformationManager.register("hdy_hdy","025177");
+            }
+        }).start();
+
+        setContentView(R.layout.activity_main);
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            String token = "pYVzEL7ZQUYEg/lE/PpzVbWtGTsXotOgj3oXNihcx7A=@gk5p.cn.rongnav.com;gk5p.cn.rongcfg.com";
+            RongIM.connect(token, new RongIMClient.ConnectCallback() {
+                @Override
+                public void onSuccess(String userId) {
+                    // 登录成功，跳转到默认会话列表页。
+                    RouteUtils.routeToConversationListActivity(MainActivity.this, "text");
+                }
+
+                @Override
+                public void onError(RongIMClient.ConnectionErrorCode connectionErrorCode) {
+
+                }
+
+                @Override
+                public void onDatabaseOpened(RongIMClient.DatabaseOpenStatus databaseOpenStatus) {
+
+                }
+            });
+        });
     }
 }
